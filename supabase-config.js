946ab3bbfcec56879fe1ab025cf1618deefdfd00
Fatma Cakmak <tc.fatmacakmak@gmail.com>
2026-09-25 16:26:55 +0300

    fixed project url.

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 /* Supabase frontend ayarları */
 window.SUPABASE_CONFIG = {
-  url: "https://vjzlifmybybeoeaexkak.supabase.co/rest/v1/",
+  url: "https://vjzlifmybybeoeaexkak.supabase.co",
   key: "sb_publishable_V2LePBpwXHBLUFRcX5c1Xw_X1qSqSRK"
 };
 // Buraya service_role/secret key KOYMAYIN.
